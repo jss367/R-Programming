eaffaceb67b0c95cfeb374f640f0f0a6a53d5ab4
Julius Simonelli <julius.simonelli@gmail.com>
2015-03-05 18:12:20 -0500

    best.R is working

--- rprog-data-ProgAssignment3-data/best.R
+++ rprog-data-ProgAssignment3-data/best.R
@@ -1,8 +1,8 @@
-#setwd('~/R/rprog-data-ProgAssignment3-data')
+#setwd('C:\Users\HMGSYS\Documents\GitHub\Classes\R-Programming\rprog-data-ProgAssignment3-data')
 best <- function(state, outcome){
         #Read outcome data
         data<- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-
+        # Determine which column in data to look at by the user input on outcome
         if (outcome == "heart attack"){
                 pointer <- 11
         } else if (outcome == "heart failure"){
@@ -12,51 +12,21 @@ best <- function(state, outcome){
         } else{
                 stop ("invalid outcome")
         }
+        #Convert that data to be numeric
         data[,pointer] <- as.numeric(data[,pointer])
-        str(data)
-        data[,pointer]<-na.omit(data[,pointer])
-        str(data)
-        
-        data[,pointer] <- as.numeric(data[,pointer])
-                x <- cbind(data[,2], data[,7], data[,pointer]) #hospital name, state, numba
-        y <- na.omit(x)
-        na.omit(x)
-        set<-FALSE
-        end <- 1
-        tops <- 100
-        valid <- FALSE
+
         #Check if the state is valid:
-        for (k in 1:nrow(y)){
-                if (y[k,2] == state){
-                        valid<-TRUE
-                }
-        }
-        if (!valid){
-                stop('invalid state')
+        if (!state %in% data$State){
+                stop("invalid state")
         }
         
-        #Find the beginning of the state you are interested in
-        for (j in 1:nrow(y)){
-                if (y[j,2] == state){
-                        if (set == FALSE){
-                                beginning <- j
-                                set <- TRUE
-                        }
-                        end <- j
-                }
-                if (set == TRUE){
-                }
-        }
-        for (k in beginning:end){
-                if (as.numeric(y[k,3]) < as.numeric(tops)){
-                      #  browser()
-                        tops <- y[k,3]
-                        ans <- y[k,1]
-                }
-        }
-        i<-1
-        print(paste0("The highest number we found is ", tops))
-        print(paste0("We believe the top institution in the state is ", ans))
+        #Get a subset of the data that contains the state you are interested in
+        correct_state <- data[data[, 7] == state, ]
+        outcome_arr <- correct_state[, pointer]
+        min <- min(outcome_arr, na.rm=T)
+        index <- which(outcome_arr == min)
+        print(correct_state[index, 2])
+        print (min)
 
         ## check that state and outcome are valid
 }
